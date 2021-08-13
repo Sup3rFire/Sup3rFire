16853f3a4fbcf5efc73f41dcc3105f851c905611
Super Fire <starjumper.pe@gmail.com>
2021-08-13 16:07:14 +0700

    Added Goals and Projects

--- pages/js/main.js
+++ pages/js/main.js
@@ -10,3 +10,27 @@ new Typed("#typed", {
   backSpeed: 50,
   typeSpeed: 70,
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const projDiv = document.getElementById("projects");
+  fetch(
+    "https://raw.githubusercontent.com/Sup3rFire/Sup3rFire/main/projects.json"
+  )
+    .then((data) => data.json())
+    .then((data) =>
+      data.projects.forEach((project, i) => {
+        if (i != 0) projDiv.innerHTML += "<hr>";
+        let addDiv = ``;
+        addDiv += `<div><h3>${project.title || ""}</h3><p>${
+          project.desc || ""
+        }</p><div id="links">`;
+        if (!!project.links) {
+          for (const key in project.links) {
+            addDiv += `<a href="${project.links[key]}">${key}</a>`;
+          }
+        }
+        addDiv += "</div></div>";
+        projDiv.innerHTML += addDiv;
+      })
+    );
+});
